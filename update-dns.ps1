c6b7f20666e0c29e5a3e7f7c3e8fe7d76a3ebd9c
liyuan-rey <10598397+liyuan-rey@users.noreply.github.com>
2025-03-14 17:53:25 +0800

    fix: 修复已有dns时更新而非新增，减少配置项

--- update-dns.ps1
+++ update-dns.ps1
@@ -87,7 +87,7 @@ try {
 	}
 	else {
 		Write-WarningLog("The .env file was not found at $envFilePath.")
-		exit 1
+		exit 2
 	}
 
 	# 获取公网 IP
@@ -101,7 +101,7 @@ try {
 	# 检查 IP 格式
 	if (-not (IsValidIPv4 -ip $publicIP)) {
 		Write-WarningLog ("$publicIP 不是有效的 IPv4 地址。")
-		exit 2
+		exit 3
 	}
 
 	Write-InfoLog("公网 IPv4 地址 : $publicIP")
@@ -109,6 +109,29 @@ try {
 	# 检查 DNS 记录是否匹配最新 IP
 	$subdomain = $ENV_SUBDOMAIN
 	$domain = $ENV_DOMAIN
+	$apiToken = $ENV_API_TOKEN
+	# $email = $ENV_EMAIL
+	# $apiKey = $ENV_API_KEY
+
+	# 构建请求头
+	$headers = @{
+		"Content-Type"  = "application/json"
+		"Authorization" = "Bearer $apiToken"
+		# "X-Auth-Email" = $email
+		# "X-Auth-Key" = "Bearer $apiKey"
+	}
+
+	
+	# 获取 zone id
+	$apiBaseUrl = "https://api.cloudflare.com/client/v4/zones"
+	$zoneIdUrl = "$($apiBaseUrl)?name=$domain"
+	$zoneIdResponse = Invoke-RestMethod -Uri $zoneIdUrl -Method Get -Headers $headers
+	$zoneId = $zoneIdResponse.result[0].id
+
+	# 获取 record id
+	$recordIdUrl = "$apiBaseUrl/$zoneId/dns_records?type=A&name=$subdomain.$domain"
+	$recordIdResponse = Invoke-RestMethod -Uri $recordIdUrl -Method Get -Headers $headers
+	$recordId = $recordIdResponse.result[0].id
 
 	try {
 		# 解析域名对应的 IP 地址
@@ -117,7 +140,7 @@ try {
 		# 遍历解析结果，检查是否有匹配的 IP 地址
 		$isMatch = $false
 		foreach ($result in $dnsResults) {
-			Write-InfoLog($result)
+			Write-InfoLog("当前解析指向： $($result.IPAddress)")
 			if ($result.IPAddress -eq $publicIP) {
 				$isMatch = $true
 				break
@@ -131,33 +154,21 @@ try {
 	}
 	catch {
 		Write-WarningLog("解析域名 $subdomain.$domain 时出现错误：$($_.Exception.Message)")
-		exit 3
+		exit 4
 	}
 	
 	# 更新 Cloudflare DNS 记录
-	$apiToken = $ENV_API_TOKEN
-	# $email = $ENV_EMAIL
-	# $apiKey = $ENV_API_KEY
-	$zoneId = $ENV_ZONE_ID
 	$ipAddress = $publicIP
 	$recordType = "A"
 	$ttl = 1
 	$proxied = $false
 
 	# 构建 API 请求 URL
-	$apiUrl = "https://api.cloudflare.com/client/v4/zones/$zoneId/dns_records"
-
-	# 构建请求头
-	$headers = @{
-		"Content-Type"  = "application/json"
-		"Authorization" = "Bearer $apiToken"
-		# "X-Auth-Email" = $email
-		# "X-Auth-Key" = "Bearer $apiKey"
-	}
+	$apiUrl = "$apiBaseUrl/$zoneId/dns_records"
 
 	# 构建请求体
 	$body = @{
-		"comment" = "auto update dns at $(Get-Date -Format "yyyy-MM-dd HH:mm:ss")"
+		"comment" = "auto update dns at $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
 		"content" = $ipAddress
 		"name"    = "$subdomain.$domain"
 		"proxied" = $proxied
@@ -165,25 +176,45 @@ try {
 		"type"    = $recordType
 	} | ConvertTo-Json
 
-	# 发送 POST 请求
+	# 更新 DNS 记录
 	try {
-		$response = Invoke-RestMethod -Uri $apiUrl -Method Post -Headers $headers -Body $body
-		if ($response.success) {
-			Write-InfoLog("DNS 记录添加成功。")
+		# 检查是否已有记录
+		$existingRecordUrl = "$apiUrl?type=$recordType&name=$subdomain.$domain"
+		$existingResponse = Invoke-RestMethod -Uri $existingRecordUrl -Method Get -Headers $headers
+		Write-InfoLog($existingResponse)
+		if ($existingResponse.result -and $existingResponse.result.Count -gt 0) {
+			# 有记录，进行更新
+			$recordId = $existingResponse.result[0].id
+			$updateUrl = "$apiUrl/$recordId"
+			$response = Invoke-RestMethod -Uri $updateUrl -Method Patch -Headers $headers -Body $body
+			if ($response.success) {
+				Write-InfoLog("DNS 记录更新成功。")
+			}
+			else {
+				Write-WarningLog("DNS 记录更新失败。错误信息: $($response.errors.message)")
+				exit 5
+			}
 		}
 		else {
-			Write-WarningLog("DNS 记录添加失败。错误信息: $($response.errors.message)")
-			exit 4
+			# 无记录，进行添加
+			$response = Invoke-RestMethod -Uri $apiUrl -Method Post -Headers $headers -Body $body
+			if ($response.success) {
+				Write-InfoLog("DNS 记录添加成功。")
+			}
+			else {
+				Write-WarningLog("DNS 记录添加失败。错误信息: $($response.errors.message)")
+				exit 6
+			}
 		}
 	}
- catch {
-		Write-WarningLog("更新 DNS 出错: $($_.Exception.Message)")
-		exit 5
+	catch {
+		Write-WarningLog("检查或更新 DNS 记录出错: $($_.Exception.Message)")
+		exit 7
 	}
 }
 catch {
 	Write-WarningLog("发生错误：$($_.Exception.Message)")
-	exit 6
+	exit 1
 }
 
 Write-InfoLog("------FINISH------")
